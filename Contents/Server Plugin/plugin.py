a56c94c8cf0c03fd0ea819984dfeefbbb1cab68b
lindehoff <jacob@cobmedia.se>
2015-11-17 10:09:16 +0100

    Removes old alarm device and code cleaning

--- Contents/Server Plugin/plugin.py
+++ Contents/Server Plugin/plugin.py
@@ -11,6 +11,7 @@ import sys
 import time
 sys.path.insert(0, '../Includes/python-verisure')
 import verisure
+import json
 
 # Note the "indigo" module is automatically imported and made available inside
 # our global name space by the host process.
@@ -60,69 +61,73 @@ class Plugin(indigo.PluginBase):
   def runConcurrentThread(self):
     try:
       while True:
-        for dev in indigo.devices.iter("self"):
-          if not dev.enabled or not dev.configured:
+        self.debugLog(u"Checking status for all Verisure Devices")
+
+        try:
+          verisure_overviews = self.myPages.get_overviews()
+          for verisure_overview in verisure_overviews:
+            self.debugLog(verisure_overview._overview_type)
+            if verisure_overview._overview_type == u"alarm":
+              for dev in indigo.devices.iter("self"):
+                if not dev.enabled or not dev.configured:
+                  continue
+                if dev.deviceTypeId == u"verisureAlarmDeviceType":
+                  dev.updateStateOnServer("status", value=verisure_overview.status)
+                  dev.updateStateOnServer("name", value=verisure_overview.name)
+                  dev.updateStateOnServer("label", value=verisure_overview.label)
+                  dev.updateStateOnServer("date", value=verisure_overview.date)
+                  #dev.updateStateOnServer("sensorValue", value=1, uiValue=verisure_overview.status)
+                  if verisure_overview.status == u"armed":
+                    dev.updateStateImageOnServer(indigo.kStateImageSel.SensorTripped)
+                  elif verisure_overview.status == u"armedhome":
+                    dev.updateStateImageOnServer(indigo.kStateImageSel.SensorOff)
+                  elif verisure_overview.status == u"unarmed":
+                    dev.updateStateImageOnServer(indigo.kStateImageSel.SensorOn)
+                  elif verisure_overview.status == u"pending":
+                    dev.updateStateImageOnServer(indigo.kStateImageSel.TimerOn)
+                  else:
+                    dev.updateStateImageOnServer(indigo.kStateImageSel.Error)
+
+                  self.debugLog("{0} {1} by {2}".format(verisure_overview.label, verisure_overview.date, verisure_overview.name))
+            if verisure_overview._overview_type == u"climate":
+              for dev in indigo.devices.iter("self"):
+                if not dev.enabled or not dev.configured:
+                  continue
+                if dev.deviceTypeId == u"verisureClimateDeviceType":
+                  if (verisure_overview.location + " (" +verisure_overview.id + ")") == dev.pluginProps["climateID"]:
+                    try:
+                      temp = verisure_overview.temperature.replace("°","").replace(",",".")
+                      input_value = float(temp)
+                      format_temp = u"%.1f"
+                      input_value = (format_temp % input_value)
+                      dev.updateStateOnServer('sensorValue', value=input_value, uiValue=input_value)
+                      dev.updateStateOnServer('temperature', value=input_value, uiValue=input_value)
+                      dev.updateStateOnServer('timestamp', value=verisure_overview.timestamp, uiValue=verisure_overview.timestamp)
+                      self.debugLog("Update temp to: {0}".format(temp))
+                      dev.updateStateImageOnServer(indigo.kStateImageSel.TemperatureSensor)
+                      dev.updateStateOnServer('onOffState', value=True, uiValue=" ")
+                    except Exception, e:
+                      self.debugLog(unicode("Unable to update device state on server. Device: %s, Reason: %s" % (dev.name, e)))
+                      dev.updateStateOnServer('sensorValue', value=u"Unsupported", uiValue=u"Unsupported")
+                      dev.updateStateImageOnServer(indigo.kStateImageSel.Error)
+        #self.debugLog(json.dumps(verisure_overviews[0]))
+        #for attr in dir(verisure_overviews[0]):
+        #  indigo.server.log("verisure_overviews.%s = %s" % (attr, getattr(verisure_overviews[0], attr)))
+        
+        except Exception, e:
+          self.debugLog(u"Error: " + str(e) + ", trying to relogging in")
+          try:
+            self.myPages.logout()
+            self.myPages = verisure.MyPages(self.pluginPrefs["verisureUsername"], self.pluginPrefs["verisurePassword"])
+            self.myPages.login()
+          except Exception, e:
+            self.debugLog(u"Unable to login, will try again in a while")
+            self.sleep(60)
             continue
-          if dev.deviceTypeId == u"verisureDeviceType":
-            self.debugLog(u"Checking status for " + dev.name)
-            try:
-              alarm_overview = self.myPages.get_overview(verisure.MyPages.DEVICE_ALARM)
-            except Exception, e:
-              self.debugLog(u"Error, trying to relogging in")
-              try:
-                self.myPages.logout()
-                self.myPages = verisure.MyPages(dev.pluginProps["verisureUsername"], dev.pluginProps["verisurePassword"])
-                self.myPages.login()
-              except Exception, e:
-                self.debugLog(u"Unable to login, will try again in a while")
-                self.sleep(60)
-
-            dev.updateStateOnServer("status", value=alarm_overview[0].status)
-            dev.updateStateOnServer("sensorValue", value=1, uiValue=alarm_overview[0].status)
-            self.debugLog("{0} {1} by {2}".format(alarm_overview[0].label, alarm_overview[0].date, alarm_overview[0].name))
-          elif dev.deviceTypeId == u"verisureAlarmDeviceType":
-            self.debugLog(u"Checking status for " + dev.name)
-            try:
-              alarm_overview = self.myPages.get_overview(verisure.MyPages.DEVICE_ALARM)
-            except Exception, e:
-              self.debugLog(u"Error, trying to relogging in")
-              try:
-                self.myPages.logout()
-                self.myPages = verisure.MyPages(self.pluginPrefs["verisureUsername"], self.pluginPrefs["verisurePassword"])
-                self.myPages.login()
-              except Exception, e:
-                self.debugLog(u"Unable to login, will try again in a while")
-                self.sleep(60)
-
-            dev.updateStateOnServer("status", value=alarm_overview[0].status)
-            dev.updateStateOnServer("name", value=alarm_overview[0].name)
-            dev.updateStateOnServer("label", value=alarm_overview[0].label)
-            dev.updateStateOnServer("date", value=alarm_overview[0].date)
-            #dev.updateStateOnServer("sensorValue", value=1, uiValue=alarm_overview[0].status)
-            self.debugLog("{0} {1} by {2}".format(alarm_overview[0].label, alarm_overview[0].date, alarm_overview[0].name))
-          elif dev.deviceTypeId == u"verisureClimateDeviceType":
-            climate_overviews = self.myPages.get_overview(verisure.MyPages.DEVICE_CLIMATE)
-            for climate_overview in climate_overviews:
-              if (climate_overview.location + " (" +climate_overview.id + ")") == dev.pluginProps["climateID"]:
-                try:
-                  temp = climate_overview.temperature.replace("°","").replace(",",".")
-                  input_value = float(temp)
-                  format_temp = u"%.1f"
-                  input_value = (format_temp % input_value)
-                  dev.updateStateOnServer('sensorValue', value=input_value, uiValue=input_value)
-                  dev.updateStateOnServer('temperature', value=input_value, uiValue=input_value)
-                  dev.updateStateOnServer('timestamp', value=climate_overview.timestamp, uiValue=climate_overview.timestamp)
-                  self.debugLog("Update temp to: {0}".format(temp))
-                except Exception, e:
-                  self.debugLog(unicode("Unable to update device state on server. Device: %s, Reason: %s" % (dev.name, e)))
-                  dev.updateStateOnServer('sensorValue', value=u"Unsupported", uiValue=u"Unsupported")
-                  dev.updateStateImageOnServer(indigo.kStateImageSel.Error)
-
-                dev.updateStateImageOnServer(indigo.kStateImageSel.TemperatureSensorOn)
-                dev.updateStateOnServer('onOffState', value=True, uiValue=" ")
         self.sleep(15)
     except self.StopThread:
       pass  # Optionally catch the StopThread exception and do any needed cleanup.
+
   def closedDeviceConfigUi(self, valuesDict, userCancelled, typeId, devId):
       """
       This method is called whenever a device config dialog is closed.
